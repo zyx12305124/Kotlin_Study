f7e8c9c2198ebdfbbee1f7ed6d939bb398a3802a
zyx12305124 <550302975@qq.com>
2021-08-20 11:13:03 +0800

    cpu密集型任务 取消

--- kotlincoroutine/app/src/test/java/com/dongnaoedu/kotlincoroutine/CoroutineTest03.kt
+++ kotlincoroutine/app/src/test/java/com/dongnaoedu/kotlincoroutine/CoroutineTest03.kt
@@ -85,6 +85,9 @@ class CoroutineTest03 {
             println("job 1")
         }
         //执行没有打印结果，因为这个GlobalScope有自己的作用域，这种方式去启动的协程没有继承父协程的上下文
+        //所以外面的父协程不会等待里面的子协程执行结束
+        //如果要等待的话就
+//         job1.join()
     }
 
     //取消异常
@@ -105,9 +108,14 @@ class CoroutineTest03 {
         //如果您不构建新的 CancellationException 实例将其作为参数传入的话，会创建一个默认的 CancellationException
         job1.cancel(CancellationException("取消 llll"))
         job1.join() //等待job1执行完
-        //job1.cancelAndJoin()
+        //job1.cancelAndJoin() ==> cancel + join
+        //我还是不不懂 cancel + join 的意义是什么
     }
 
+
+    /**
+     * 可以被取消的 挂起函数 withContext delay
+     */
     //CPU密集型任务取消
     //isActive是一个可以被使用在CoroutineScope中的扩展属性，检查Job是否处于活跃状态。
     @Test
@@ -128,8 +136,38 @@ class CoroutineTest03 {
         delay(1300L)
         println("main: I'm tired of waiting!")
         job.cancelAndJoin() // 取消一个作业并且等待它结束
+        //在讲job的生命周期的时候，job取消或者协程运行出错的时候任务为取消中（canceling），isActive = false,isCancelled = true;
+        //当所有子协程都完成后，协程会进入已取消 Cancelled 状态，此时isCompleted = true
+        //也就是说当 job.cancelAndJoin()  被调用的时候 isActive 变成了 false
+        //所以这里用job.cancel + isActive 也可以达到停止cpu密集型任务的效果
+        // 如`test cancel cpu task by isActive 0` 没有isActive拦截就会打印五次 无法被取消
         println("main: Now I can quit.")
     }
+
+    @Test
+    fun `test cancel cpu task by isActive 0`() = runBlocking {
+        val startTime = System.currentTimeMillis()
+        //CPU密集型任务在Default调度器中运行，在主线程中通过isActive取消不了
+        val job = launch(Dispatchers.Default){
+            var nextPrintTime = startTime
+            var i = 0
+            while(i < 5 ){
+                //每秒打印消息两次
+                if(System.currentTimeMillis() >= nextPrintTime){
+                    println("job: I'm sleeping ${i++} ...")
+                    nextPrintTime += 500L
+                }
+            }
+        }
+        delay(1300L)
+        println("main: I'm tired of waiting!")
+        job.cancelAndJoin() // 取消一个作业并且等待它结束
+        println("main: Now I can quit.")
+        //最后打印5次
+
+//        问题：为什么CPU密集型任务不能被取消？ 这是协程的一个保护机制，
+    }
+
 
 
     //ensureActive()，如果job处于非活跃状态，这个方法会立即抛出异常。
@@ -140,7 +178,8 @@ class CoroutineTest03 {
             var nextPrintTime = startTime
             var i = 0
             while(i < 5){
-                ensureActive()
+                ensureActive()//点进源码 这里本质还是通过isActive去判断
+                // 上面那种是悄无声息的结束了job 这种方式还会抛出一个异常告诉调用者 可以被catch到的
                 if(System.currentTimeMillis() >= nextPrintTime){
                     println("job: I'm sleeping ${i++} ...")
                     nextPrintTime += 500L
@@ -154,6 +193,7 @@ class CoroutineTest03 {
     }
 
 
+
     //yield函数会检查所在协程的状态，如果已经取消，则抛出CancellationException予以响应。
     //此外，它还会尝试出让线程的执行权，给其他协程提供执行机会。
     //如果要处理的任务属于：
@@ -165,7 +205,7 @@ class CoroutineTest03 {
             var nextPrintTime = startTime
             var i = 0
             while(i < 5){
-                yield()
+                yield()//出让线程执行权
                 if(System.currentTimeMillis() >= nextPrintTime){
                     println("job: I'm sleeping ${i++} ...")
                     nextPrintTime += 500L
